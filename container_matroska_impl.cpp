9ba203e933531e1885bb238f3e226d6a8623ab96
Matt <mattgruenke@users.noreply.github.com>
2017-02-13 01:05:44 -0500

    Build fixes for Linux, and without foobar2000.

--- container_matroska_impl.cpp
+++ container_matroska_impl.cpp
@@ -4,34 +4,35 @@
  * container_matroska
  */
 
-void container_matroska_impl::open(const char * p_path, bool p_info_only, foobar2000_io::abort_callback &p_abort) {
+void container_matroska_impl::open(const char * p_path, bool p_info_only, abort_callback &p_abort) {
     cleanup();
     if (!is_our_path(p_path)) {
-        throw exception_io_unsupported_format();
+//        throw exception_io_unsupported_format();  TO_DO
     }
     m_path = p_path;
     m_abort = &p_abort;
-    service_ptr_t<file> file_ptr;
+    std::ifstream file(m_path.c_str());
+//    if (!file) throw exception_io_unsupported_format();   TO_DO
     try {
-        filesystem::g_open_read(file_ptr, m_path, *m_abort);
-        matroska_parser_ptr parser = matroska_parser_ptr(new MatroskaAudioParser(file_ptr, *m_abort));
+        matroska_parser_ptr parser = matroska_parser_ptr(new MatroskaAudioParser(file, *m_abort));
         parser->Parse(p_info_only);
-        for (t_size i = 0; i != parser->GetAttachmentList().get_count(); ++i) {
-            MatroskaAttachment & item = parser->GetAttachmentList().get_item(i);
-            matroska::attachment attachment(this, *m_abort, item.FileName.GetUTF8().c_str(), item.MimeType.c_str(), item.Description.GetUTF8().c_str(),
-                static_cast<t_size>(item.SourceDataLength), static_cast<t_sfilesize>(item.SourceStartPos));
-            m_attachment_list.add_item(attachment);
+        MatroskaAudioParser::attachment_list &attachments = parser->GetAttachmentList();
+        for (MatroskaAudioParser::attachment_list::iterator item = attachments.begin(); item != attachments.end(); ++item) {
+            matroska::attachment attachment(this, *m_abort, item->FileName.GetUTF8().c_str(), item->MimeType.c_str(), item->Description.GetUTF8().c_str(),
+                static_cast<size_t>(item->SourceDataLength), static_cast<size_t>(item->SourceStartPos));
+            m_attachment_list.push_back(attachment);
         }
     } catch (...) {
-        throw exception_io_unsupported_format();
+//        throw exception_io_unsupported_format();  TO_DO
     }
 }
 
-void container_matroska_impl::open_file(service_ptr_t<file> & p_out, const filesystem::t_open_mode p_mode) const {
-    filesystem::g_open(p_out, m_path, p_mode, *m_abort);
+void container_matroska_impl::open_file(std::fstream & p_out, const std::ios_base::openmode p_mode) const {
+    p_out.open(m_path.c_str(), p_mode);
+//    if (!p_out) m_abort();    TO_DO
 }
 
-void container_matroska_impl::get_display_path(pfc::string_base & p_out) const {
+void container_matroska_impl::get_display_path(std::string & p_out) const {
     p_out = m_path;
 }
 
