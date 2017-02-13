24727aa12a4f1f323a43a0fd27726aa0e8d30d4d
Matt <mattgruenke@users.noreply.github.com>
2017-02-13 00:43:36 -0500

    More build fixes, on Linux & without foobar2000.

--- container_matroska_impl.h
+++ container_matroska_impl.h
@@ -1,6 +1,9 @@
 #ifndef _CONTAINER_MATROSKA_IMPL_H_
 #define _CONTAINER_MATROSKA_IMPL_H_
 
+#include <boost/filesystem/path.hpp>
+#include <boost/algorithm/string/case_conv.hpp>
+
 #include "matroska_parser.h"
 #include "container_matroska.h"
 
@@ -9,15 +12,15 @@ typedef boost::shared_ptr<MatroskaAudioParser> matroska_parser_ptr;
 class container_matroska_impl : public container_matroska
 {
 private:
-    typedef pfc::list_t<matroska::attachment> attachment_list_impl;
+    typedef std::list<matroska::attachment> attachment_list_impl;
 
     abort_callback * m_abort;
-    pfc::string8 m_path;
+    std::string m_path;
     attachment_list_impl m_attachment_list;
 
     void cleanup() {
-        m_path.reset();
-        m_attachment_list.remove_all();
+        m_path.clear();
+        m_attachment_list.clear();
     };
 
 protected:
@@ -29,17 +32,15 @@ protected:
 
 public:
     virtual void open(const char * p_path, bool p_info_only, abort_callback & p_abort);
-    virtual void open_file(service_ptr_t<file> & p_out, const filesystem::t_open_mode p_mode = filesystem::open_mode_read) const;
-    virtual void get_display_path(pfc::string_base & p_out) const;
+    virtual void open_file(std::fstream & p_out, const std::ios_base::openmode p_mode = std::ios_base::in) const;
+    virtual void get_display_path(std::string & p_out) const;
     virtual bool is_our_path(const char * p_path) const {
-        if (stricmp_utf8(pfc::string_extension(p_path), "mka") != 0 && stricmp_utf8(pfc::string_extension(p_path), "mkv") != 0) {
-            return false;
-        }
-        return true;
+        std::string ext = boost::algorithm::to_lower_copy(
+            boost::filesystem::path(p_path).extension().string() );
+        return (ext == "mka" || ext == "mkv");
     }
     virtual const matroska::attachment_list * get_attachment_list() const;
 };
 
-static container_matroska_factory_t<container_matroska_impl> g_container_matroska_impl_factory;
 
 #endif // _CONTAINER_MATROSKA_IMPL_H_
