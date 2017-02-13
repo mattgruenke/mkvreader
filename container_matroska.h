c46875055bda42c095f157f69393cd5f9f02d701
Matt <mattgruenke@users.noreply.github.com>
2017-02-13 00:01:56 -0500

    More build fixes.

--- container_matroska.h
+++ container_matroska.h
@@ -2,6 +2,8 @@
 #define _CONTAINER_MATROSKA_H_
 
 #include <list>
+#include <string>
+#include <fstream>
 
 namespace foobar2000_io {
 
@@ -17,15 +19,16 @@ namespace foobar2000_io {
     class container_matroska : public service_base {
     public:
         virtual void open(const char * p_path, bool p_info_only, abort_callback & p_abort)=0;
-        virtual void open_file(service_ptr_t<file> & p_out, const filesystem::t_open_mode p_mode = filesystem::open_mode_read) const =0;
-        virtual void get_display_path(pfc::string_base & p_out) const =0;
+        virtual void open_file(std::fstream &p_out, const std::ios_base::openmode p_mode = std::ios_base::in) const =0;
+        virtual void get_display_path(std::string & p_out) const =0;
         virtual bool is_our_path(const char * p_path) const =0;
         virtual const matroska::attachment_list * get_attachment_list() const =0;
 
     public:
-        static void g_open(service_ptr_t<container_matroska> & p_out, const char * p_path, bool p_info_only, abort_callback & p_abort) {
+        static void g_open(boost::shared_ptr<container_matroska> & p_out, const char * p_path, bool p_info_only, abort_callback & p_abort) {
+#if 0  // TO_DO
             service_enum_t<container_matroska> e;
-            service_ptr_t<container_matroska> ptr;
+            boost::shared_ptr<container_matroska> ptr;
             while(e.next(ptr)) {
                 if (ptr->is_our_path(p_path)) {
 		            ptr->open(p_path, p_info_only, p_abort);
@@ -34,54 +37,57 @@ namespace foobar2000_io {
                 }
             }
 	        throw exception_io_unsupported_format();
+#endif
         }
         static bool g_is_our_path(const char * p_path) {
+#if 0  // TO_DO
             service_enum_t<container_matroska> e;
-            service_ptr_t<container_matroska> ptr;
+            boost::shared_ptr<container_matroska> ptr;
             while(e.next(ptr)) {
                 if (ptr->is_our_path(p_path)) {
 		            return true;
                 }
             }
+#endif
             return false;
         }
-
-        FB2K_MAKE_SERVICE_INTERFACE_ENTRYPOINT(container_matroska);
     };
 
-    typedef service_ptr_t<container_matroska> container_matroska_ptr;
+    typedef boost::shared_ptr<container_matroska> container_matroska_ptr;
 
     namespace matroska {
         class attachment {
         private:
             container_matroska * m_owner;
             abort_callback * m_abort;
-            pfc::string8 m_name, m_mime_type, m_description;
-            t_size m_size;
-            t_sfilesize m_position;
+            std::string m_name, m_mime_type, m_description;
+            size_t m_size;
+            size_t m_position;
 
         public:
             attachment() : m_owner(0), m_abort(0) {};
             attachment(const container_matroska_ptr & p_owner, abort_callback & p_abort, const char * p_name, const char * p_mime_type,
-                        const char * p_description, const t_size p_size, const t_sfilesize p_position)
+                        const char * p_description, const size_t p_size, const size_t p_position)
                 : m_name(p_name), m_mime_type(p_mime_type), m_description(p_description), m_size(p_size), m_position(p_position)
             {
-                m_owner = p_owner.get_ptr();
+                m_owner = p_owner.get();
                 m_abort = &p_abort;
             };
             ~attachment() {};
-            void get_name(pfc::string_base & p_out) const { p_out = m_name; };
-            void get_mime_type(pfc::string_base & p_out) const { p_out = m_mime_type; };
-            void get_description(pfc::string_base & p_out) const { p_out = m_description; };
-            t_size get_size() const { return m_size; };
-            t_sfilesize get_position() const { return m_position; };
-            void get(void * p_buffer, t_sfilesize p_position, t_size p_size) const {
-                service_ptr_t<file> file_ptr;
-                m_owner->open_file(file_ptr);
-                file_ptr->seek_ex(p_position, file::seek_from_beginning, *m_abort);
-                file_ptr->read(p_buffer, p_size, *m_abort);
+            void get_name(std::string & p_out) const { p_out = m_name; };
+            void get_mime_type(std::string & p_out) const { p_out = m_mime_type; };
+            void get_description(std::string & p_out) const { p_out = m_description; };
+            size_t get_size() const { return m_size; };
+            size_t get_position() const { return m_position; };
+            void get(void * p_buffer, size_t p_position, size_t p_size) const {
+                std::fstream file;
+                m_owner->open_file(file);
+                file.seekp(p_position);
+                // if (file) m_abort();     TO_DO
+                file.read(static_cast<char *>(p_buffer), p_size);
+                // if (file) m_abort();     TO_DO
             };
-            void get(void * p_buffer, t_size p_size = 0) const {
+            void get(void * p_buffer, size_t p_size = 0) const {
                 if (!p_size) {
                     p_size = get_size();
                 }
@@ -93,12 +99,5 @@ namespace foobar2000_io {
 };
 
 using namespace foobar2000_io;
-
-template<typename T>
-class container_matroska_factory_t : public service_factory_t<T> {};
-
-// {32491C6C-F941-42cf-8519-EBF5061BB088}
-FOOGUIDDECL const GUID container_matroska::class_guid = 
-{ 0x32491c6c, 0xf941, 0x42cf, { 0x85, 0x19, 0xeb, 0xf5, 0x6, 0x1b, 0xb0, 0x88 } };
 
 #endif // _CONTAINER_MATROSKA_H_
