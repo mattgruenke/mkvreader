ca9a839990e4d12fd0a60a5efa66971669b73857
Matt <mattgruenke@users.noreply.github.com>
2017-02-26 13:04:29 -0500

    Simple fixes.

--- filesystem_matroska.h
+++ filesystem_matroska.h
@@ -1,49 +1,42 @@
 #ifndef _FOO_INPUT_MATROSKA_FILESYSTEM_MATROSKA_H_
 #define _FOO_INPUT_MATROSKA_FILESYSTEM_MATROSKA_H_
 
-#include "../SDK/foobar2000.h"
+#include <fstream>
+
 #include "container_matroska.h"
 
-/*
-class NOVTABLE filesystem_matroska_interface : public filesystem {
-	FB2K_MAKE_SERVICE_INTERFACE(filesystem_matroska_interface,filesystem);
-};
 
-// {67456A35-7324-448e-8442-CD997FCCCC74}
-FOOGUIDDECL const GUID filesystem_matroska_interface::class_guid = 
-{ 0x67456a35, 0x7324, 0x448e, { 0x84, 0x42, 0xcd, 0x99, 0x7f, 0xcc, 0xcc, 0x74 } };
-*/
-
-class NOVTABLE filesystem_matroska : public filesystem {
+class filesystem_matroska /* : public filesystem */ {
 public:
-	virtual bool get_canonical_path(const char * p_path,pfc::string_base & p_out);
+	virtual bool get_canonical_path(const char * p_path, std::string & p_out);
 	virtual bool is_our_path(const char * p_path);
-	virtual bool get_display_path(const char * p_path,pfc::string_base & p_out);
+	virtual bool get_display_path(const char * p_path, std::string & p_out);
 
-	virtual void open(service_ptr_t<file> & p_out,const char * p_path, t_open_mode p_mode,abort_callback & p_abort);
+	virtual void open(std::fstream & p_out,const char * p_path, std::ios_base::openmode p_mode, abort_callback & p_abort);
     virtual void remove(const char * p_path,abort_callback & p_abort);
     virtual void move(const char * p_src,const char * p_dst,abort_callback & p_abort);
 	//! Queries whether a file at specified path belonging to this filesystem is a remove object or not.
 	virtual bool is_remote(const char * p_src);
 
 	//! Retrieves stats of a file at specified path.
-	virtual void get_stats(const char * p_path,t_filestats & p_stats,bool & p_is_writeable,abort_callback & p_abort);
+//	virtual void get_stats(const char * p_path, t_filestats & p_stats, bool & p_is_writeable,abort_callback & p_abort);
 	
-	virtual bool relative_path_create(const char * file_path,const char * playlist_path,pfc::string_base & out) {return 0;}
-	virtual bool relative_path_parse(const char * relative_path,const char * playlist_path,pfc::string_base & out) {return 0;}
+	virtual bool relative_path_create(const char * file_path,const char * playlist_path, std::string & out) {return 0;}
+	virtual bool relative_path_parse(const char * relative_path,const char * playlist_path, std::string & out) {return 0;}
 
 	//! Creates a directory.
-	virtual void create_directory(const char * p_path,abort_callback & p_abort);
+//	virtual void create_directory(const char * p_path, abort_callback & p_abort);
 
-	virtual void list_directory(const char * p_path,directory_callback & p_out,abort_callback & p_abort);
+//	virtual void list_directory(const char * p_path, directory_callback & p_out, abort_callback & p_abort);
 
 	//! Hint; returns whether this filesystem supports mime types.
 	virtual bool supports_content_types();
 
 private:
-    inline bool get_source_file_path(const char * p_path, pfc::string_base & p_out)
+/*
+    inline bool get_source_file_path(const char * p_path, std::string & p_out)
     {
-        pfc::string8 path, file;
+        std::string path, file;
         if (archive_impl::g_parse_unpack_path(p_path, path, file)) {
             path << "|" << file;
         } else {
@@ -51,16 +44,17 @@ private:
         }
         path.remove_chars(0, 11);
         path.truncate(path.find_last('|'));
-        pfc::string8 ext(pfc::string_extension(path).get_ptr());
+        std::string ext(pfc::string_extension(path).get_ptr());
         if (filesystem::g_is_recognized_path(path)) {
             p_out = path;
             return true;
         }
         return false;
     }
-    inline bool get_attachment_file_name(const char * p_path, pfc::string_base & p_out)
+
+    inline bool get_attachment_file_name(const char * p_path, std::string & p_out)
     {
-        pfc::string8 path, file;
+        std::string path, file;
         if (archive_impl::g_parse_unpack_path(p_path, path, file)) {
             path << "|" << file;
         } else {
@@ -81,9 +75,10 @@ private:
         }
         return false;
     }
+*/
 
 public:
-    static void g_make_matroska_path(pfc::string_base & path, const char * file, const char * name)
+    static void g_make_matroska_path(std::string & path, const char * file, const char * name)
     {
 	    path = "matroska://";
 	    path += file;
@@ -92,9 +87,5 @@ public:
     }
 };
 
-template<typename T>
-class filesystem_matroska_factory_t : public service_factory_single_t<T> {};
-
-static filesystem_matroska_factory_t<filesystem_matroska> g_filesystem_matroska_factory;
 
 #endif//_FOO_INPUT_MATROSKA_FILESYSTEM_MATROSKA_H_
